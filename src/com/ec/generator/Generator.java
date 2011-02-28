4fc583e6edb60ac56e5ab2c2438685d4e4cc8d36
Raunak Gupta <raunak.gupta@hotmail.com>
2011-02-28 15:37:52 +0000

    class is now abstract with abstract methods that are shared among sub classes

--- src/com/ec/generator/Generator.java
+++ src/com/ec/generator/Generator.java
@@ -2,13 +2,20 @@ package com.ec.generator;
 
 import java.util.Random;
 
+import com.ec.Individual;
 import com.ec.node.Node;
 
 public abstract class Generator{
-	/** */
+	
+	/** Default probablity for stoping grow tree*/
 	protected static float growStopProbability = 0.25f;
 	
-	/** */
+	/** A static instance of <code>Random</code>*/
 	protected static Random random = new Random();
+	
+	public abstract Node getRandomTerminal();
+	public abstract Node fullTree(int depth);
+	public abstract Node growTree(int depth);
+	public abstract Individual fitness(Node node);
 	
 }
