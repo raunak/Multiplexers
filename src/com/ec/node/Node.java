b1de492a8167b2078310bda86f0efcb518ffb6a5
Raunak Gupta <raunak.gupta@hotmail.com>
2011-03-03 17:30:52 +0000

    documented some parts of the code

--- src/com/ec/node/Node.java
+++ src/com/ec/node/Node.java
@@ -19,45 +19,69 @@ package com.ec.node;
 import java.util.Vector;
 
 /**
- * @version 1.0 
+ * @version 1.0
  */
 public abstract class Node {
-	
+
 	/** Parent node */
 	public Node parent;
-	
+
 	/** Children of a Node */
 	public Vector<Node> children;
-	
+
 	/** returns max depth of Node */
 	public abstract int getDepth();
-	
+
 	/** evaluates a program for a given input */
 	public abstract boolean eval(int input);
-	
-	/** returns an vector will all the child nodes*/
+
+	/** returns an vector will all the child nodes */
 	public abstract Vector<Node> enumerate();
-	
-	public abstract Vector<Node> enumBounded(int remainingDepth, int subtreeDepth);
+
+	/** */
+	public abstract Vector<Node> enumBounded(int remainingDepth,
+			int subtreeDepth);
 
 	/** returns the number of nodes */
 	public abstract int countNodes();
 
+	/**
+	 * Gets the level of <code>Node</code> relative to root node.
+	 * 
+	 * @return int
+	 */
 	public int getLevel() {
-		if (parent == null ) return 0;
-		else return parent.getLevel() +1;
+		if (parent == null)
+			return 0;
+		else
+			return parent.getLevel() + 1;
 	}
 
-	/** returns children for a node */
+	/**
+	 * Gets children of a <code>Node</code>. 
+	 * 
+	 * @return children
+	 */
 	public Vector<Node> getChildren() {
 		return children;
 	}
-	
-	public Node getParent(){
+
+	/**
+	 * Gets the parent of a <code>Node</code>
+	 * 
+	 * @return parent
+	 */
+	public Node getParent() {
 		return parent;
 	}
-	
-	public void setParent(Node node){
-		this.parent = node;
+
+	/**
+	 * Set the parent of a <code>Node</code>
+	 * 
+	 * @param parent
+	 *            - <code>Node</code> Object
+	 */
+	public void setParent(Node parent) {
+		this.parent = parent;
 	}
 }
